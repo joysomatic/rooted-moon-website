d6e9a893ed53f704513d379704402257e5387b18
jose onate <onarson@gmail.com>
2026-09-03 15:04:35 -0700

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // or 'hybrid' depending on your rendering preference
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
